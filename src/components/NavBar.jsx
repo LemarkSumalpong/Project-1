ab6a85801ec4b6a9ee1ed0cffb76e763491033e2
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-08-31 21:22:56 +0800

    hahahaha

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,56 @@
-import Logo1 from '../assets/Logo1.png';
 
-function NavBar() {
+
+import { Search } from 'lucide-react';
+import LogoFinal from '../assets/LogoFinal.png';
+const NavItems = [
+  { name: 'Home', href: '#home'},
+  { name: 'Shop', href: '#Shop' },
+  {
+    name: 'Category',
+    href: '#category',
+    subItems: [
+      { name: 'Books', href: '#book' },
+      { name: 'Electronics', href: '#electronics' },
+      { name: 'Clothing', href: '#clothing' },
+      { name: 'Accessories', href: '#accessories' },
+      { name: 'Sales', href: '#sales' },
+    ],
+  },
+  { name: 'Cart', href: '#cart' },
+];
+export const NavBar = () => {
   return (
-    <div>
-      <a href="#">
-        <img src={Logo1} alt="logo" />
-      </a>
-    </div>
+    <nav className="flex items-center justify-between px-6 py-4 ">
+      <img src={LogoFinal} alt="logo" />
+      <ul className="flex space-x-6">
+        {NavItems.map((item) => (
+          <li key={item.name} className='relative group'>
+            <a href={item.href} className="hover:text-blue">
+              {item.name}
+            </a>
+            {item.subItems && (
+              <ul className="absolute left-0 mt-2 hidden group-hover:block text-block rounded-full">
+                {item.subItems.map((sub) => (
+                  <li key={sub.name}>
+                    <a
+                      href={sub.href}
+                      className="block px-4 py-2 hover:bg-blue-200"
+                    >
+                      {sub.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </li>
+        ))}
+      </ul>
+      <div className="relative flex w-full gap-2 md:w-max">
+        <Search size={20} / >
+        <input type="text" placeholder="search here" className='px-3 py-1 border border-gray-300 rounded-full'></input>
+      </div>
+    </nav>
   );
-}
+};
 
 export default NavBar;
