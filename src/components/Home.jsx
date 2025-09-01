3212c6eab51046dc8e74512495c3a814d720baa6
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-09-01 22:06:46 +0800

    okay na to good night

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+export const  Home = () => {
+    return(
+         <div 
+      className="h-screen w-screen bg-cover bg-center"
+      style={{ backgroundImage: "url('/background.jpg')" }}
+    >
+    </div>
+    )
+}
+export default Home;
